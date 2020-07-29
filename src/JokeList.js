0e4924de4acbecac7cbab5c916362d44474706b9
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-07-29 18:24:35 +0300

    Fetch joke from api

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class JokeList extends Component {
+  async componentDidMount() {
+    let res = await axios.get("https://icanhazdadjoke.com/", {
+      headers: { Accept: "application/json" },
+    });
+    console.log(res.data.joke);
+  }
+
   render() {
     return <h1>Dad Jokes</h1>;
   }
