a5e8095bf0f0a9157fb7d8b4752a40c43f5570fb
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-07-30 18:37:33 +0300

    Add loading icon

--- src/JokeList.js
+++ src/JokeList.js
@@ -14,6 +14,7 @@ class JokeList extends Component {
 
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+      loading: false,
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -37,6 +38,7 @@ class JokeList extends Component {
     this.setState(
       (st) => ({
         jokes: [...st.jokes, ...jokes],
+        loading: false,
       }),
       () =>
         window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
@@ -56,10 +58,18 @@ class JokeList extends Component {
   }
 
   handleClick() {
-    this.getJokes();
+    this.setState({ loading: true }, this.getJokes);
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div className="JokeList-spinner">
+          <i className="far fa-8x fa-laugh fa-spin"></i>
+          <h1 className="JokeList-title">Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
