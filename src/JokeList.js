b96e04752302424cc89560e91c0572d8fadc6c86
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-07-30 18:26:53 +0300

    Add new jokes to existing ones

--- src/JokeList.js
+++ src/JokeList.js
@@ -15,6 +15,7 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -33,8 +34,13 @@ class JokeList extends Component {
       jokes.push({ text: res.data.joke, votes: 0, id: uuidv4() });
     }
 
-    this.setState({ jokes });
-    window.localStorage.setItem("jokes", JSON.stringify(jokes));
+    this.setState(
+      (st) => ({
+        jokes: [...st.jokes, ...jokes],
+      }),
+      () =>
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
   }
 
   handleVote(id, delta) {
@@ -49,6 +55,10 @@ class JokeList extends Component {
     );
   }
 
+  handleClick() {
+    this.getJokes();
+  }
+
   render() {
     return (
       <div className="JokeList">
@@ -57,7 +67,9 @@ class JokeList extends Component {
             <span>Dad</span> Jokes
           </h1>
           <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>
+            New Jokes
+          </button>
         </div>
         <div className="JokeList-list">
           {this.state.jokes.map((joke) => (
